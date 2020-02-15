3676df74d0507eaa08105a800a5ac9bb305e2658
Harvey Jarvis <hpjarvis@live.co.uk>
2020-02-15 15:50:00 +0000

    Added helpful comments

--- window_placer.py
+++ window_placer.py
@@ -4,15 +4,17 @@ import re
 
 def resize_discord_chrome():
     # Windows that will be placed on second monitor
+    # Get all open windows
     target_windows = []
-
     windows = gw.getAllTitles()
 
     for window_name in windows:
+    #   if window is Google Chrome or Discord
         if "Google Chrome" in window_name:
             target_windows.append(window_name)
             current_window = gw.getWindowsWithTitle(window_name)[0]
-
+#           If window is on second screen
+#           If window is not maximized
             if (current_window.top < 0 and current_window.left < 0):
                 if current_window.isMaximized == True:
                     current_window.minimize()
@@ -26,7 +28,6 @@ def resize_discord_chrome():
             current_window.resizeTo(1080, 850)
             current_window.moveTo(-1080, 559)
 
-    print(target_windows)
 
 if __name__ == "__main__":
     print("Hello World")
