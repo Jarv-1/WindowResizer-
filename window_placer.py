4b586b2313d2327300c16e51118ce742ab67286a
Harvey Jarvis <hpjarvis@live.co.uk>
2020-02-15 15:14:22 +0000

    Position Chrome and discord in place

---
+++ window_placer.py
@@ -0,0 +1,21 @@
+import pygetwindow as gw
+import re
+
+# Windows that will be placed on second monitor
+target_windows = []
+
+windows = gw.getAllTitles()
+
+for window_name in windows:
+    if "Google Chrome" in window_name:
+        target_windows.append(window_name)
+        current_window = gw.getWindowsWithTitle(window_name)[0]
+        current_window.resizeTo(1096, 950)
+        current_window.moveTo(-1088, -383)
+    elif "Discord" in window_name:
+        target_windows.append(window_name)
+        current_window = gw.getWindowsWithTitle(window_name)[0]
+        current_window.resizeTo(1080, 850)
+        current_window.moveTo(-1080, 559)
+
+print(target_windows)
